91d540f0a1b095f344f71bc371be4ffb0786c6f6
Devaradhan Murugesan <demuruge@microsoft.com>
2019-08-05 17:44:59 -0700

    adding serializable objects for axe configuration

---
+++ Selenium.Axe/Selenium.Axe/AxeContextOptions.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.Axe
+{
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public class AxeContextOptions
+    {
+        [JsonProperty("include")]
+        public string[][] Include { get; set; }
+        [JsonProperty("exclude")]
+        public string[][] Exclude { get; set; }
+    }
+}
---
+++ Selenium.Axe/Selenium.Axe/AxeRunOptions.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Selenium.Axe
+{
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public class RunOnlyOptions
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("values")]
+        public List<string> Values { get; set; }
+
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public class RuleOptions
+    {
+        [JsonProperty("enabled")]
+        public bool? Enabled { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public class AxeRunOptions
+    {
+        [JsonProperty("runOnly")]
+        public RunOnlyOptions RunOnly { get; set; }
+
+        [JsonProperty("rules")]
+        public Dictionary<string, RuleOptions> Rules { get; set; }
+
+    }
+}
